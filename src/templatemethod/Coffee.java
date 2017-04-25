a2aad2384c5a4243b8bc96078f807b18d3a2c84d
ZhangShenao <15201440436@163.com>
2017-04-25 13:57:40 +0800

    commit 13

--- src/templatemethod/Coffee.java
+++ src/templatemethod/Coffee.java
@@ -1,4 +1,4 @@
-package template;
+package templatemethod;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/templatemethod/DuckSorter.java
+++ src/templatemethod/DuckSorter.java
@@ -1,4 +1,4 @@
-package template;
+package templatemethod;
 
 import java.util.Arrays;
 
